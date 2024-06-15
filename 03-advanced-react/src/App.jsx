7cfdba4136e9a7dd01d46c69feca721cf3605189
TanLe <tan.lesi@study.hs-duesseldorf.de>
2024-06-15 23:10:05 +0200

    progress

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,6 +1,6 @@
 import './index.css'
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function"
-import Final from "./tutorial/02-useEffect/final/05-cleanup-function"
+import Starter from "./tutorial/06-forms/starter/02-user-challenge"
+import Final from "./tutorial/06-forms/final/02-user-challenge"
 
 function App() {
   return (
--- 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UserChallenge = () => {
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState(data);
+  const addUser = (e) => {
+    e.preventDefault();
+    if (!name) return;
+    const newUser = { id: Date.now(), name: name };
+    setUsers([...users, newUser]);
+    setName("");
+  }
+  const removeUser = (name) => {
+    setUsers(
+      users.filter((user) => user.name !== name)
+    )
+  };
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={addUser} >
         <h4>Add User</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input type='text' className='form-input'
+            id='name' value={name} onChange={(e) => setName(e.target.value)} />
         </div>
 
         <button type='submit' className='btn btn-block'>
@@ -15,7 +33,17 @@ const UserChallenge = () => {
         </button>
       </form>
       {/* render users below */}
-    </div>
+      {
+        users.map((user) => {
+          return (
+            <div key={user.id}>
+              <h4>{user.name}</h4>
+              <button type="button" className="btn" onClick={() => removeUser(user.name)}>Remove</button>
+            </div>
+          )
+        })
+      }
+    </div >
   );
 };
 export default UserChallenge;
