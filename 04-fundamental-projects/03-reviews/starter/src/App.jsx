8b023c4dc2f7d722ac1fce971f73d43411bec720
tlesi <tlesi@scalefree.com>
2024-06-18 15:56:12 +0200

    progress project 3

--- 04-fundamental-projects/03-reviews/starter/src/App.jsx
+++ 04-fundamental-projects/03-reviews/starter/src/App.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import Reviews from "./Reviews";
+import reviews from "./data"
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
-};
+  const [idx, setIdx] = useState(0);
+  return (
+    <div>
+      {
+        reviews.map((review) => {
+          return (
+            <div>
+              <Reviews key={review.id} review={review} />
+              <div className="button-container">
+                <button className="prev-btn" type="button">Prev</button>
+                <button className="next-btn" type="button">Next</button>
+              </div>
+            </div>
+          )
+        }
+        )
+      }
+    </div>
+  )
+}
 export default App;
