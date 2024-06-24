fdfa785646db0eaac0b07d431dd1f1d0d4d19f69
tlesi <tlesi@scalefree.com>
2024-06-24 16:46:27 +0200

    progress project 8

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,37 @@
+import { useState } from "react";
+import text from "./data";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [amount, setAmount] = useState(1);
+  const [displayText, setDisplayText] = useState([]);
+  const handleSummit = (e) => {
+    e.preventDefault();
+    const selectedText = text.slice(0, amount);
+    setDisplayText(selectedText);
+    setAmount(1);
+  }
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSummit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input type="number" name="amount" id="amount"
+          min={1} max={8} step={1} value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button type="submit" className="btn">generate</button>
+      </form>
+      {
+        displayText.map((txt) => {
+          return (
+            <article className="lorem-text">
+              <p>{txt}</p>
+            </article>
+          )
+        })
+      }
+    </section>
+
+  )
 };
 export default App;
